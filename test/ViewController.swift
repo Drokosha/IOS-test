c63d2e4f0f6c79531cee9a914905eb1f6a57f8ea
Drokosha <drokok@mail.ru>
2022-06-15 17:35:00 +0300

    Upfield

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -7,31 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     let solo = UITextField()
+    let overlayButton = UIButton(type: .custom)
+    let bookmarkImage = UIImage(systemName: "bookmark")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let overlayButton = UIButton(type: .custom)
-        let bookmarkImage = UIImage(systemName: "infoButton")
+        
         overlayButton.setImage(bookmarkImage, for: .normal)
+        overlayButton.backgroundColor = .red
         overlayButton.addTarget(self, action: #selector(displayBookmarks),
             for: .touchUpInside)
         overlayButton.sizeToFit()
         
-        self.solo.frame = CGRect(x: 50, y: 50, width: 140, height: 50)
-        self.solo.text = "Hello"
-        //self.solo.becomeFirstResponder()
-        self.solo.backgroundColor = .red
+        
+        self.solo.frame = CGRect(x: 50, y: 50, width: 200, height: 30)
+        self.solo.text = "Hello;l,'';,';,';,';,';,;',';;,';,'"
+        self.solo.adjustsFontSizeToFitWidth = true
+        self.solo.tintColor = .red
+        self.solo.textAlignment = .center
+        self.solo.placeholder = "Твой будущий текст"
+        self.solo.font = UIFont(name: "Helvetica Nueu", size: 80)
+        self.solo.borderStyle = .bezel
+        self.solo.keyboardAppearance = .dark
+        self.solo.keyboardType = .emailAddress
+        self.solo.delegate = self
         self.solo.leftView = overlayButton
         self.solo.leftViewMode = .always
+        self.solo.clearButtonMode = .always
         self.view.addSubview(solo)
-        self.view.addSubview(solo)
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        self.solo.resignFirstResponder()
+        return true
     }
     
     @objc func displayBookmarks () {
-        
+        print("Получилось")
     }
 
 
